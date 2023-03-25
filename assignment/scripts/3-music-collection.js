aadd0b594336b63caec91ec0de6b9f26f180dbf7
Japheth Yang <japheth720@gmail.com>
2023-03-25 12:34:00 -0500

    Made collection variable with addToCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,15 @@
 console.log('***** Music Collection *****')
+
+let collection = [];
+
+function addToCollection(title, artist, yearPublished) {
+
+};
+
+const albumOne = {
+    title: 'Yeah im a frog';
+    artist: 'Heng Yang';
+    yearPublished: '2022';
+
+};
+
